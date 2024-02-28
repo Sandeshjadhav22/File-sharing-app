696fc7878c1fbcdb70cf6cecfbd7fa2b428456d3
sandy <sandeshjadhav2002@gmail.com>
2024-02-28 22:55:25 +0530

    Data saving in database

--- app/(dashboard)/(routes)/upload/page.jsx
+++ app/(dashboard)/(routes)/upload/page.jsx
@@ -8,11 +8,17 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { useUser } from "@clerk/nextjs";
+import { genrateRandomeString } from "@/app/_utils/GenrateRandomString";
 
 function Upload() {
+  const {user} = useUser();
   const [progress, setProgress] = useState();
 
   const storage = getStorage(app);
+  const db =getFirestore(app)
+  const [uploadCompleted, setUploadCompleted] = useState(false)
   const uploadFile = (file) => {
     const metadata = {
       contentType: file.type,
@@ -29,9 +35,26 @@ function Upload() {
       progress == 100 &&
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log("File available at", downloadURL);
+          saveInfo(file,downloadURL);
         });
     });
-  };
+  };  
+
+  const saveInfo= async(file,fileUrl)=>{
+    const docId =genrateRandomeString().toString();
+    await setDoc(doc(db, "uploadedFile", docId), {
+      fileName: file?.name,
+      fileSize: file?.size,
+      fileType: file?.type,
+      fileUrl:fileUrl,
+      userEmail:user?.primaryEmailAddress.emailAddress,
+      userName:user?.fullName,
+      password:'',
+      id:docId,
+      shortUrl:process.env.NEXT_PUBLIC_BASE_URL+docId
+    });
+  }
+
   return (
     <div className="p-5 px-8 md:px-28">
       <h2 className="text-[20px] text-center m-5">
