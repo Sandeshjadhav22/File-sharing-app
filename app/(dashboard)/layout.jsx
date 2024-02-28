c5b22db7b6d0f5287e3f0f15892e982b03290f88
sandy <sandeshjadhav2002@gmail.com>
2024-02-28 13:42:10 +0530

    sideBar done

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import SideNav from "./_components/SideNav";
+import TopHeader from "./_components/TopHeader";
 
-function layout({children}) {
+function layout({ children }) {
   return (
     <div>
-        {children}
-        </div>
-  )
+      <div className=" h-full w-64 flex-col fixed inset-y-0 z-50 md:flex hidden">
+        <SideNav />
+      </div>
+      <div className="md:ml-64">
+        <TopHeader/>
+        {children}</div>
+    </div>
+  );
 }
 
-export default layout
+export default layout;
